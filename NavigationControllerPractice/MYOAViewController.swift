a72264d80e247e378c4b61544f3693e286889698
Youngsun Paik <youngsunpaik@gmail.com>
2016-04-10 01:33:59 -0700

    Added func startOver to make the rightBarButtonItem functional

--- NavigationControllerPractice/MYOAViewController.swift
+++ NavigationControllerPractice/MYOAViewController.swift
@@ -16,9 +16,11 @@ class MYOAViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: .Bordered , target: self, action: "startOver")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func startOver() {
+        if let navigationController = self.navigationController {
+            navigationController.popToRootViewControllerAnimated(true)
+//            navigationController.popViewControllerAnimated(true)
+        }
     }
 
 
